cedc9871ca73332dd9b1e5da4bcdf35c672f4153
karen0431 <49350616+karen0431@users.noreply.github.com>
2019-04-06 20:29:28 -0700

    First commit
    Uploading all the files

---
+++ Apr6practice/src/randomInput.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class randomInput {
+
+	public static void main(String[] args) {
+		Scanner userInputScanner = new Scanner(System.in);
+		System.out.println("Please choose a letter from these options: r, a, t");
+		String letter = userInputScanner.next();
+
+		if (letter.equals("r") ||letter.equals("a") || letter.equals("t")) {
+			System.out.println("The letter you chose is " + letter);
+		} 
+		else {
+			System.out.println("Please try again");
+		}
+	}
+}
